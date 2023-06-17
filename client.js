f23ce43bfc800ae93e91ee0b00c6b959d9f67fda
Atarax665 <manasgmishra@gmail.com>
2023-06-17 14:50:23 +0530

    most freq left

---
+++ client.js
@@ -0,0 +1,128 @@
+const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+const SERVER_URL = 'http://localhost:5000';
+
+function addFiles(filenames) {
+  const form = new FormData();
+  for (const filename of filenames) {   
+    const fileStream = fs.createReadStream(filename);
+    form.append('files', fileStream);
+  }
+  axios
+    .post(`${SERVER_URL}/add`, form, {
+      headers: form.getHeaders(),
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error('Error:', error.response.data.error);
+    });
+}
+
+function listFiles() {
+  axios
+    .get(`${SERVER_URL}/list`)
+    .then((response) => {
+      console.log(response.data.files);
+    })
+    .catch((error) => {
+      console.error('Error:', error.response.data.error);
+    });
+}
+
+function removeFile(filename) {
+  axios
+    .delete(`${SERVER_URL}/remove?filename=${filename}`, {
+      data: { filename },
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error('Error:', error.response.data.error);
+    });
+}
+
+function updateFile(filename) {
+  const form = new FormData();
+  const fileStream = fs.createReadStream(filename);
+  form.append('file', fileStream);
+  form.append('filename', filename);
+  axios
+    .put(`${SERVER_URL}/update`, form, {
+      headers: form.getHeaders(),
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error('Error:', error.response.data.error);
+    });
+}
+
+function wordCount() {
+  axios
+    .get(`${SERVER_URL}/wc`)
+    .then((response) => {
+        console.log(response.data);
+    //   console.log(`Total word count: ${response.data.word_count}`);
+    })
+    .catch((error) => {
+      console.error('Error:', error.response.data.error);
+    });
+}
+
+function frequentWords(limit = 10, order = 'asc') {
+  axios
+    .get(`${SERVER_URL}/freq-words?limit=${limit}&order=${order}`)
+    .then((response) => {
+      console.log(`Most frequent words (limit=${limit}, order=${order}):`);
+      console.log(response.data.freq_words);
+    })
+    .catch((error) => {
+      console.error('Error:', error.response.data.error);
+    });
+}
+
+function parseArguments() {
+  const args = process.argv.slice(2);
+  const command = args[0];
+
+  switch (command) {
+    case 'add':
+      const filenames = args.slice(1);
+      addFiles(filenames);
+      break;
+    case 'ls':
+      listFiles();
+      break;
+    case 'rm':
+      const filename = args[1];
+      removeFile(filename);
+      break;
+    case 'update':
+      const updateFilename = args[1];
+      updateFile(updateFilename);
+      break;
+    case 'wc':
+      wordCount();
+      break;
+    case 'freq-words':
+      const limit = parseInt(args[1]) || 10;
+      const order = args[2] || 'asc';
+      frequentWords(limit, order);
+      break;
+    default:
+      console.error('Invalid command');
+      break;
+  }
+}
+
+parseArguments();
+
+// Asc cat * | tr -s ' ' '\n' | sort | uniq -c | sort -n | tail -n 10
+// Desc cat files/* | tr -s ' ' '\n' | sort | uniq -c | sort -nr | head -n 10
---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+const app = express();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'files');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage });
+
+const fileStore = {};
+
+app.post('/add', upload.array('files'), (req, res) => {
+  const files = req.files;
+  for (const file of files) {
+    const { originalname, path } = file;
+    if (fileStore[originalname]) {
+      return res.json({ error: 'File already exists' });
+    }
+    fileStore[originalname] = path;
+  }
+  return res.json({ message: 'Files added successfully' });
+});
+
+app.get('/list', (req, res) => {
+  const files = Object.keys(fileStore);
+  return res.json({ files });
+});
+
+app.delete('/remove', (req, res) => {
+  const filename = req.query.filename;
+  if (fileStore[filename]) {
+    fs.unlinkSync(fileStore[filename]); 
+    delete fileStore[filename];
+    return res.json({ message: 'File removed successfully' });
+  } else {
+    return res.json({ error: 'File not found' });
+  }
+});
+
+app.put('/update', upload.single('file'), (req, res) => {
+  const { filename } = req.body;
+  const { originalname, path } = req.file;
+  if (fileStore[filename]) {
+    delete fileStore[filename];
+  }
+  fileStore[originalname] = path;
+  return res.json({ message: 'File updated successfully' });
+});
+
+app.get('/wc', (req, res) => {
+    wc_count = {};
+    for (const filename of Object.values(fileStore)) {
+        const wcCommand = `cat ${filename} | wc -w`;
+        const output = execSync(wcCommand).toString().trim();
+        newfilename = filename.split('/')[1];
+        wc_count[newfilename] = parseInt(output);
+    }
+    return res.json({ word_count: wc_count});
+});
+
+app.get('/freq-words', (req, res) => {
+  const limit = parseInt(req.query.limit) || 10;
+  const order = req.query.order || 'asc';
+  if (order == 'asc') {
+    freqWordsCommand = `cat files/* | tr -s ' ' '\n' | sort | uniq -c | sort -n | tail -n ${limit}`;
+    } else {
+    freqWordsCommand = `cat files/* | tr -s ' ' '\n' | sort | uniq -c | sort -nr | tail -n ${limit}`;
+    }
+  const output = execSync(freqWordsCommand).toString().trim();
+  console.log(output);
+  return res.json({ freq_words: output});
+//   const freqWords = output
+//     .split('\n')
+//     .map((line) => line.split(' ').slice(1).join(' ').trim());
+//   return res.json({ freq_words: freqWords });
+});
+
+function populateFileStore() {
+    fs.readdirSync('files').forEach((filename) => {
+        fileStore[filename] = `files/${filename}`;
+    });
+}
+
+populateFileStore();
+console.log(fileStore);
+
+app.listen(5000, () => {
+  console.log('File store server is running on http://localhost:5000');
+});
